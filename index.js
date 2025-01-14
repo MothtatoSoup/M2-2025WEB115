bbe4a496a2e12fbdc0daec89b8cc350a8c15a242
MothtatoSoup <maddy.puryear@outlook.com>
2025-01-14 13:11:34 -0500

    Learnt more things yippeeeee

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // L O G S //
 
-console.log('This message will show up in the console! Yippeee')
-console.log(document) // Document means HTML, this will show the html code in the console
+//console.log('This message will show up in the console! Yippeee')
+//console.log(document) // Document means HTML, this will show the html code in the console
 //console.dir(document) // more in depth 
 
 
@@ -19,15 +19,15 @@ console.log(document) // Document means HTML, this will show the html code in th
 
 // W R I T E   T O   I D //
 
-document.getElementById("myH1").innerHTML = "I Love JavaScript" ; // gets the header and ovverides it with this
-document.getElementById("myP").textContent = "YIPPPPEEEEEEEEEEEEEEEEEE" ; // does the same as ^ 
+//document.getElementById("myH1").innerHTML = "I Love JavaScript" ; // gets the header and ovverides it with this
+//document.getElementById("myP").textContent = "YIPPPPEEEEEEEEEEEEEEEEEE" ; // does the same as ^ 
 
 
 
 // S T Y L I N G   P A G E   E L E M E N T S //
 
-document.body.style.color = "purple"
-document.body.style.backgroundColor = "black"
+//document.body.style.color = "purple"
+//document.body.style.backgroundColor = "black"
 //document.body.style.fontSize = "2em"
 
 
@@ -45,11 +45,34 @@ document.body.style.backgroundColor = "black"
 //document.write(`<p>hello ${student}</p>`) // Writes to docuemnt but doesnt work with script differ
 
 // New Ver (Modern web dev)
-const student = prompt("What is ur name?")
-const messageElement = document.createElement('p') //Creating new 'p' element
-messageElement.textContent = `Hello, ${student}` // Sets text content of the new 'p' element
-document.body.appendChild(messageElement) // ataches the element to the end of the doc
+//const student = prompt("What is ur name?")
+//const messageElement = document.createElement('p') //Creating new 'p' element
+//messageElement.textContent = `Hello, ${student}` // Sets text content of the new 'p' element
+//document.body.appendChild(messageElement) // ataches the element to the end of the doc
 // This ensures the content is added w/o overidding any page content
 
 
 // E V E N T   L I S T E N E R S //
+document.getElementById("myH1").addEventListener("click", function(){
+    // what happens when you click the header
+    document.getElementById("myH1").style.color = "red"
+})
+
+document.getElementById("myH1").addEventListener("mouseover", function(){
+    document.getElementById("myH1").style.color = "purple"
+})
+
+document.getElementById("myP").addEventListener("dblclick",function(){
+    document.getElementById("myP").style.fontSize = "2em"
+})
+
+document.getElementById("MyButton").addEventListener("click", function(){
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+})
+
+// D I F F   B E T W E E N .innerHTML and .textContent //
+
+// .textContent would output: This is a paragraph!
+// .innerHTML would output: <p>This is a paragraph!<p>
+// If getting input, use textContent. If you use innerHTML, ppl could type in html code and the site would acccept it and run it! 
